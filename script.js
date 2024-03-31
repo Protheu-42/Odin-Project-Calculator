c66abd6204eda220b016abc15498d41e66f9ab24
Protheu-42 <everaldo.sobrinho42@gmail.com>
2024-03-31 13:35:59 -0300

    4 Basic operations create

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+// Basic Math Operations
+function add(firstNumber , secondNumber){
+    return firstNumber + secondNumber;
+};
+
+function subtract(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
+};
+
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
+};
+
+function divide(firstNumber, secondNumber){
+    return secondNumber === 0 ? 0 : firstNumber / secondNumber;
+};
